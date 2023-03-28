9855f66df0b64bbe2fae1b32daed5ba2b35ad99a
Rodrigo <rodrigoml2395@gmail.com>
2023-03-28 14:48:39 -0300

    feat: frontend dockerfile

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,13 +3,14 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || 'password',
   database: 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
+    decimalNumbers: true,
   },
   logging: false,
 }

c3b6d608ac64dad8e066b64853d20e78f785a940
Rodrigo <rodrigoml2395@gmail.com>
2023-03-28 16:05:15 -0300

    test: Get a team

--- app/backend/src/tests/teamController.test.ts
+++ app/backend/src/tests/teamController.test.ts
@@ -22,4 +22,18 @@ describe('Testing team controller', () => {
       expect(response.body).to.be.deep.equal(teams);
     });
   });
+  describe('Listing a team', function () {
+    it('Should return status 200 and team', async () => {
+      Sinon.stub(Team, 'findByPk').resolves(teams[0]);
+      const response: Response = await chai.request(app).get('/teams/1');
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal(teams[0]);
+    });
+    it('Should return an error with a nonexistent id', async () => {
+      Sinon.stub(Team, 'findByPk').resolves(null);
+      const response: Response = await chai.request(app).get('/teams/19');
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.be.deep.equal({ message: 'Team not found!' });
+    });
+  });
 });
